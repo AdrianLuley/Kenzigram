2ff1103826ba3ed0de69aceff49788750a4d3e32
mdshepard <mdshepard8@gmail.com>
2018-04-25 14:20:14 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'public/uploads' })
+const port = 3000;
+const app = express();
+const fs = require('fs');
+
+const uploaded_files = [];
+
+app.use(express.static('public'));
+
+app.listen(port);
+
+app.post('/upload', upload.single('myFile'), function (req, res, next) {
+    const path = './public/uploads';
+    fs.readdir(path, function(err, items) {
+        let html = "";
+        for(let i=0; i < items.length; i++){
+            html += `<img src="${"./uploads/" + items[i]}">`;
+        }
+        console.log(items);
+        res.send(html);
+    });
+    // req.file is the `myFile` file
+    // req.body will hold the text fields, if there were any
+  })
+
