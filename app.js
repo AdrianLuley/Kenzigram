806ec0d361fa3e1e6a536d88895091827d0fb161
mdshepard <mdshepard8@gmail.com>
2018-04-30 10:10:35 -0400

    Pug implemented. Cleaned up.

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const multer = require('multer');
+
 const upload = multer({ dest: 'public/uploads' })
-const port = 3000;
 const app = express();
 const fs = require('fs');
 
+const port = 3000;
 const uploaded_files = [];
+
+app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
+app.post('/upload', upload.single('myFile'), function (req, res) {
+    let html = '<a href="http://localhost:3000"> Go Back Home </a>';
+    res.send(html);
+    });
+
+app.get('/', function(req, res) {
+    const path = './public/uploads';
+    fs.readdir(path, function (err, items) {
+        console.log(items);
+        res.render('index', {images: items});
+    });
+});
+
 app.listen(port);
 
-app.post('/upload', upload.single('myFile'), function (req, res, next) {
-    const path = './public/uploads';
-    fs.readdir(path, function(err, items) {
-        let html = '<a href="http://localhost:3000"> Go Back Home';
-        for(let i=0; i < items.length; i++){
-            html += `<img src="${"./uploads/" + items[i]}">`;
-        }
-        console.log(items);
-        res.send(html);
-    });
-    // req.file is the `myFile` file
-    // req.body will hold the text fields, if there were any
-  });
 
+// app.get('/', function (req, res) {
+//     app.render('index', { title: "Kenziegram Upload", h1: "Kenziegram", a: "href=http://localhost:3000" })
+// });
